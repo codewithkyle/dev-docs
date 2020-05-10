401369104730c66f79ff75ed7ce84d82d6a4b131
Kyle Andrews <kylea@page.works>
2020-05-10 11:30:00 -0400

    Closed #1 - added static rendering output functionality

---
+++ cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const path = require("path");
+const semver = require("semver");
+const glob = require("glob");
+
+/** Verify Nodejs version */
+const packageJson = require(path.join(__dirname, "package.json"));
+const version = packageJson.engines.node;
+if (!semver.satisfies(process.version, version)) {
+    const rawVersion = version.replace(/[^\d\.]*/, "");
+    console.log(`Dev Docs requires at least Node v${rawVersion} and you're using Node ${process.version}`);
+    process.exit(1);
+}
+
+const yargs = require("yargs").argv;
+const mode = yargs?.o ? 0 : 1;
+
+if (mode) {
+    const Server = require("./server");
+    new Server();
+} else {
+    const cwd = process.cwd();
+    const fs = require("fs");
+    const path = require("path");
+
+    const output = path.resolve(cwd, yargs.o);
+    if (fs.existsSync(output)) {
+        fs.rmdirSync(output, { recursive: true });
+    }
+    fs.mkdirSync(output);
+
+    const docs = path.resolve(cwd, "docs");
+    const navUtil = require("./lib/navigation");
+    const nav = navUtil.buildNavigation(docs);
+    const renderer = require("./lib/renderer");
+
+    for (let i = 0; i < nav.length; i++) {
+        const outputFilePath = path.join(output, nav[i].slug);
+        fs.mkdirSync(outputFilePath, { recursive: true });
+
+        let doc = renderer.renderFile(nav[i].file);
+        doc = renderer.renderNavigation(doc, nav);
+        fs.writeFileSync(path.join(outputFilePath, "index.html"), doc);
+
+        if (nav[i].slug === "/readme") {
+            fs.writeFileSync(path.join(output, "index.html"), doc);
+        }
+    }
+
+    const staticCSSFiles = glob.sync(path.join(__dirname, "static", "*.css"));
+    for (let i = 0; i < staticCSSFiles.length; i++) {
+        const filename = staticCSSFiles[i].replace(/.*[\/\\]/g, "");
+        fs.copyFileSync(staticCSSFiles[i], path.join(output, filename));
+    }
+
+    const staticJSFiles = glob.sync(path.join(__dirname, "static", "*.js"));
+    for (let i = 0; i < staticJSFiles.length; i++) {
+        const filename = staticJSFiles[i].replace(/.*[\/\\]/g, "");
+        fs.copyFileSync(staticJSFiles[i], path.join(output, filename));
+    }
+}
--- server.js
+++ server.js
@@ -1,112 +1,112 @@
-#!/usr/bin/env node
-
 const fs = require("fs");
 const path = require("path");
-const semver = require("semver");
 const express = require("express");
 const watch = require("watch");
-const os = require("os");
 const boxen = require("boxen");
 
 const cwd = process.cwd();
-const interfaces = os.networkInterfaces();
 
-/** Verify Nodejs version */
-const packageJson = require(path.join(__dirname, "package.json"));
-const version = packageJson.engines.node;
-if (!semver.satisfies(process.version, version)) {
-    const rawVersion = version.replace(/[^\d\.]*/, "");
-    console.log(`Dev Docs requires at least Node v${rawVersion} and you're using Node ${process.version}`);
-    process.exit(1);
-}
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = 5000;
+        this.interfaces = require("os").networkInterfaces();
 
-const getNetworkAddress = () => {
-    for (const name of Object.keys(interfaces)) {
-        for (const interface of interfaces[name]) {
-            const { address, family, internal } = interface;
-            if (family === "IPv4" && !internal) {
-                return address;
+        this.docs = path.join(cwd, "docs");
+        this.readme = path.join(this.docs, "readme.md");
+        this.normalizeCssPath = path.resolve(cwd, "node_modules/normalize.css/normalize.css");
+        this.navUtil = require("./lib/navigation");
+        this.nav = this.navUtil.buildNavigation(this.docs);
+        this.renderer = require("./lib/renderer");
+
+        this.init();
+        this.startServer();
+        this.watch();
+    }
+
+    init() {
+        if (!fs.existsSync(this.docs)) {
+            fs.mkdirSync(this.docs);
+        }
+
+        if (!fs.existsSync(this.readme)) {
+            fs.writeFileSync(this.readme, "Start here");
+            fs.copyFileSync(path.join(__dirname, "readme.md"), this.readme);
+        }
+
+        if (!fs.existsSync(path.join(__dirname, "static", "normalize.css"))) {
+            fs.copyFileSync(this.normalizeCssPath, path.join(__dirname, "static", "normalize.css"));
+        }
+    }
+
+    watch() {
+        watch.createMonitor(this.docs, (monitor) => {
+            monitor.on("created", () => {
+                this.nav = this.navUtil.buildNavigation(this.docs);
+            });
+            monitor.on("removed", () => {
+                this.nav = this.navUtil.buildNavigation(this.docs);
+            });
+        });
+    }
+
+    startServer() {
+        try {
+            this.app.listen(this.port, () => {
+                let message = "";
+                message += `Local: http://localhost:${this.port}\n`;
+                message += `Network: http://${this.getNetworkAddress()}:${this.port}`;
+                console.log(
+                    boxen(message, {
+                        padding: 1,
+                        borderColor: "green",
+                        margin: 1,
+                    })
+                );
+            });
+        } catch {
+            console.error(`Port ${this.port} was taken, trying port ${this.port + 1}`);
+            this.port++;
+            this.startServer();
+            return;
+        }
+
+        this.app.use(express.static(path.join(__dirname, "static")));
+
+        this.app.get("/", (req, res) => {
+            let doc = this.renderer.renderFile(this.readme);
+            doc = this.renderer.renderNavigation(doc, this.nav);
+            res.send(doc);
+        });
+
+        this.app.get("/*", (req, res) => {
+            const slug = decodeURI(req.url);
+            let file = null;
+            for (let i = 0; i < this.nav.length; i++) {
+                if (this.nav[i].slug === slug) {
+                    file = this.nav[i].file;
+                    break;
+                }
+            }
+            if (file) {
+                let doc = this.renderer.renderFile(file);
+                doc = this.renderer.renderNavigation(doc, this.nav);
+                res.send(doc);
+            } else {
+                res.status(404).send("404 | Document Not Found");
+            }
+        });
+    }
+
+    getNetworkAddress() {
+        for (const name of Object.keys(this.interfaces)) {
+            for (const iface of this.interfaces[name]) {
+                const { address, family, internal } = iface;
+                if (family === "IPv4" && !internal) {
+                    return address;
+                }
             }
         }
     }
-};
-
-const app = express();
-let port = 5000;
-function startServer() {
-    try {
-        app.listen(port, () => {
-            let message = "";
-            message += `Local: http://localhost:${port}\n`;
-            message += `Network: http://${getNetworkAddress()}:${port}`;
-            console.log(
-                boxen(message, {
-                    padding: 1,
-                    borderColor: "green",
-                    margin: 1,
-                })
-            );
-        });
-    } catch {
-        console.error(`Port ${port} was taken, trying port ${port + 1}`);
-        port++;
-        startServer();
-    }
 }
-startServer();
-
-const docs = path.join(cwd, "docs");
-if (!fs.existsSync(docs)) {
-    fs.mkdirSync(docs);
-}
-
-const readme = path.join(docs, "readme.md");
-if (!fs.existsSync(readme)) {
-    fs.writeFileSync(readme, "Start here");
-    fs.copyFileSync(path.join(__dirname, "readme.md"), readme);
-}
-
-const normalizeCssPath = path.resolve(cwd, "node_modules/normalize.css/normalize.css");
-if (!fs.existsSync(path.join(__dirname, "static", "normalize.css"))) {
-    fs.copyFileSync(normalizeCssPath, path.join(__dirname, "static", "normalize.css"));
-}
-
-const navUtil = require("./lib/navigation");
-let nav = navUtil.buildNavigation(docs);
-
-watch.createMonitor(docs, (monitor) => {
-    monitor.on("created", () => {
-        nav = navUtil.buildNavigation(docs);
-    });
-    monitor.on("removed", () => {
-        nav = navUtil.buildNavigation(docs);
-    });
-});
-
-app.use(express.static(path.join(__dirname, "static")));
-
-const renderer = require("./lib/renderer");
-
-app.get("/", function (req, res) {
-    let doc = renderer.renderFile(readme);
-    doc = renderer.renderNavigation(doc, nav);
-    res.send(doc);
-});
-
-app.get("/*", function (req, res) {
-    const slug = decodeURI(req.url);
-    let file = null;
-    for (let i = 0; i < nav.length; i++) {
-        if (nav[i].slug === slug) {
-            file = nav[i].file;
-            break;
-        }
-    }
-    if (file) {
-        let doc = renderer.renderFile(file);
-        doc = renderer.renderNavigation(doc, nav);
-        res.send(doc);
-    } else {
-        res.status(404).send("404 | Document Not Found");
-    }
-});
+module.exports = Server;
